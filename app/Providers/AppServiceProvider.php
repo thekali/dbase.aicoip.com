81043ff4e518e2257427ef77f20c6af30a4a4ed4
thekali <thekali@icloud.com>
2026-04-17 17:37:54 +0200

    JS_Setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use Override;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\ServiceProvider;
+use Override;
 
 class AppServiceProvider extends ServiceProvider
 {
